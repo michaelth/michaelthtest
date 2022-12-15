fa9057cdb9090eacd10cd9cdd192ab268668db69
Michael Theys <michael.th90@gmail.com>
2022-12-15 08:57:51 +0100

    updated test

--- test-apikey.py
+++ test-apikey.py
@@ -1,29 +1,12 @@
-import os
-import openai
-import wandb
+import hashlib
+import binascii
 
+def process_request(request):
+    password = request.GET["password"]
 
-openai.api_key = "eBG00lPrVLPgiv/NVehnHYe49c9cHqDY+DNZJRAte3Rk2QaI2BxjCuYmmwRTtUwD"
-run = wandb.init(project='GPT-3 in Python')
-prediction_table = wandb.Table(columns=["prompt", "completion"])
+    # BAD: Inbound authentication made by comparison to string literal
+    if password == "myPa55word":
+        redirect("login")
 
-run = wandb.init(project='GPT-3 in Python')
-prediction_table = wandb.Table(columns=["prompt", "completion"])
-gpt_prompt = "Correct this to standard English:\n\nShe no went to the market."
-
-
-response = openai.Completion.create(
-  engine="text-davinci-002",
-  prompt=gpt_prompt,
-  temperature=0.5,
-  max_tokens=256,
-  top_p=1.0,
-  frequency_penalty=0.0,
-  presence_penalty=0.0
-)
-
-
-print(response['choices'][0]['text'])
-
-
-prediction_table.add_data(gpt_prompt,response['choices'][0]['text'])
+    hashed_password = load_from_config('hashed_password', CONFIG_FILE)
+    salt = load_from_config('salt', CONFIG_FILE)
